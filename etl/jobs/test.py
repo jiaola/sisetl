6d1b18e3c5138048df5d7f07d28b43ac0a530d12
Dazhi Jiao <djiao@jhu.edu>
2019-04-09 07:05:31 -0400

    Add a test job

---
+++ etl/jobs/test.py
@@ -0,0 +1,29 @@
+import bonobo
+import requests
+
+FABLABS_API_URL = 'https://public-us.opendatasoft.com/api/records/1.0/search/?dataset=fablabs&rows=1000'
+
+
+def extract_fablabs():
+    yield from requests.get(FABLABS_API_URL).json().get('records')
+
+
+def get_graph(**options):
+    graph = bonobo.Graph()
+    graph.add_chain(
+        extract_fablabs,
+        bonobo.Limit(2),
+        bonobo.PrettyPrinter(),
+    )
+    return graph
+
+
+def get_services(**options):
+    return {}
+
+
+if __name__ == '__main__':
+    parser = bonobo.get_argument_parser()
+    with bonobo.parse_args(parser) as options:
+        print(options)
+        bonobo.run(get_graph(**options), services=get_services(**options))
